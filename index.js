c4ca50e89aea4cc03b3e58a9b9645a4724b94dc0
achaltelmasre <achaltelmasre@gmail.com>
2023-10-19 12:41:22 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import  express  from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 5000;
+
+app.get('/health', (req, res) => {
+    res.json({ status: 'All good'});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
+});
