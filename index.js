3380dfa33206833ab54fe4d92afbcf1475e49d26
achaltelmasre <achaltelmasre@gmail.com>
2023-10-21 18:52:20 +0530

    data saved parmently

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import  express  from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import Student from './src/models/student.js';
 
 const app = express();
 
@@ -6,96 +11,97 @@ app.use(express.json());
 
 const PORT = 5000;
 
-const students = [];
-
-app.get('/health', (req, res) => {
-    res.json({ status: 'All good!, All set'});
-});
-
-app.get('/students', (req, res) => {
-
-  res.json({
-    success:true,
-    data: students,
-    message: 'Successfully fetched all students',
-  })
-});
-
-app.post('/student', (req, res) => {
-  const {name,age,mobile,email} = req.body;
-
-  if (!name) {
-     return res.json({
-      success: false,
-      message: 'Name is required',
-     })
-  }
-  if (!age) {
-    return res.json({
-     success: false,
-     message: 'Age is required',
-    })
- }
-
- if (!mobile) {
-  return res.json({
-   success: false,
-   message: 'Mobile is required',
-  })
-}
-
-if (!email) {
-  return res.json({
-   success: false,
-   message: 'Email is required',
-  })
-}
-  const id = Math.floor(Math.random() * 100000) + 1;
-
-
-  
-  const newStudent = {
-     id,
-    name,
-    age,
-    mobile,
-     email,
-  }
-
-  students.push(newStudent);
-
-  res.json({
-    success: true,
-    data: newStudent,
-    message: 'Successfully added new student',
-  })
-})
-
-app.get('/student', (req,res)=>{
-  const {id} = req.query;
-
-  let student = null;
-
-  students.forEach((stud)=>{
-    if(stud.id == id){
-      student = stud;
+const connectMongoDB = async () =>{
+   const conn = await mongoose.connect(process.env.MONGODB_URI)
+    if (conn) {
+       console.log('MongoDB connected successfully');   
     }
-  })
+};
+connectMongoDB();
 
-  if(student == null){
-    return res.json({
-      success: false,
-      message: 'Student not found',
+app.get('/health', (req,res) =>{
+    res.json({status: 'All good , All set'});
+});
+
+app.get('/students', async (req, res) =>{
+
+    const students = await Student.find()
+
+    res.json({
+       success: true,
+       data: students,
+       massage: 'successfully fetched all students',
     })
-  }
+});
 
-  res.json({
-    success: true,
-    data: student,
+app.post('/student', async (req, res) =>{
+    const {name, age, mobile,email} = req.body;
+
+    if (!name) {
+      return res.json({
+            success:false,
+            message: 'Name is required',
+        })
+    }
+
+    if (!name) {
+        return res.json({
+              success:false,
+              message: 'Name is required',
+          })
+      }
+
+      if (!age) {
+        return res.json({
+              success:false,
+              message: 'Age is required',
+          })
+      }
+
+      if (!mobile) {
+        return res.json({
+              success:false,
+              message: 'Mobile is required',
+          })
+      }
+
+      if (!email) {
+        return res.json({
+              success:false,
+              message: 'Email is required',
+          })
+      }
+
+   const stud = new Student({
+     name:name,
+     age:age,
+     mobile: mobile,
+     email:email,
+   });
+
+   const savedStudent = await stud.save();
+
+    res.json({
+        success: true,
+        data:savedStudent,
+        message: 'Successfully added new student',
+    })
+});
+
+//Get Student
+app.get('/student', async (req, res) => {
+   const {email} = req.query;
+   
+   const student = await Student.findOne({email:email});
+
+   res.json({
+    success:true,
+    data:student,
     message: 'Successfully fetched student',
-  })
-})
+   })
+});
 
-app.listen(PORT, () => {
-  console.log('Server is running on port ${PORT}.');
+
+app.listen(PORT, () =>{
+    console.log(` Server running on port ${PORT}`);
 });
